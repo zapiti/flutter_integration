ef245494a6d0b798d8003ea81d2525e3210f852c
nathan ranghel <nathan.oliveira@midway.com.br>
2021-07-21 14:20:42 -0300

    att

--- lib/integration_test/main_driver.dart
+++ lib/integration_test/main_driver.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_integration/main.dart' as app;
 
 void mainDriver(Function(WidgetTester) callBack) {
